5a9fc7acf5a6facd30040b03df0f2ec878cfdab9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-04-25 14:26:37 +0000

    build based on f82b097

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BasicBSpline.jl-1","page":"Home","title":"BasicBSpline.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [BasicBSpline]","category":"page"}]
+}
