0b9659e199c177a351ee17d89674daf3f58124a2
hyrodium <hyrodium@gmail.com>
2020-04-26 15:33:47 +0900

    add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using BasicBSpline
+using IntervalSets
 using Test
 
 @testset "BasicBSpline.jl" begin
     # Write your own tests here.
+
+    @testset "Knots" begin
+        @test zero(Knots)==Knots([])
+        @test Knots(1:3)==Knots([3,2,1])
+        @test Knots([-1,2,3])+2Knots([2,5])==Knots([-1,2,2,2,3,5,5])
+        @test Knots([-1,2,3])+2Knots([2,5])==Knots([-1,2,2,2,3,5,5])
+    end
+
+    @testset "BSplineSpace" begin
+        P = BSplineSpace(2,Knots([1,3,5,6,8,9]))
+        @test BSplineSupport(2,P)==3..8
+        @test dim(P)==3
+    end
+
 end
