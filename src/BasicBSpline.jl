65dae73cff0682a6baf90c65587ca4ad62d10815
hyrodium <hyrodium@gmail.com>
2021-12-20 21:56:46 +0900

    update includes

--- src/BasicBSpline.jl
+++ src/BasicBSpline.jl
@@ -9,7 +9,7 @@ export Point
 export Knots, ♯, BSplineSpace, dim, ⊑, ⊒, ⋢, ⋣, ≃
 export bsplinebasis₊₀, bsplinebasis₋₀, bsplinebasis
 export bsplinebasis′₊₀, bsplinebasis′₋₀, bsplinebasis′
-export bsplinesupport, bsplineunity, changebasis
+export bsplinesupport, bsplineunity, changebasis, lower
 export BSplineManifold, refinement, bsplinespaces, controlpoints
 export isproper, properdim
 export degree, knots
@@ -23,12 +23,12 @@ const MAX_DEGREE = 5
 
 include("_Knots.jl")
 include("_BSplineSpace.jl")
-include("_ChangeBasis.jl")
 include("_BSplineBasis.jl")
-include("_FastBSplineSpace.jl")
-include("_FastBSplineBasis.jl")
+include("_ChangeBasis.jl")
+include("_FastBSplineSpace.jl")  # TODO: remove this
+include("_FastBSplineBasis.jl")  # TODO: remove this
 include("_BSplineManifold.jl")
-include("_FastBSplineManifold.jl")
+include("_FastBSplineManifold.jl")  # TODO: remove this
 include("_Refinement.jl")
 include("_Integral.jl")
 include("_Fitting.jl")
