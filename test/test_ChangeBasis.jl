7c3e3f0951ca7ab4eaa90c422151eb2593ffaf50
hyrodium <hyrodium@gmail.com>
2021-12-20 22:00:57 +0900

    update tests for ChangeBasis

--- test/test_ChangeBasis.jl
+++ test/test_ChangeBasis.jl
@@ -29,13 +29,20 @@
     @test P5 ⋢ P4
     @test P4 ⋣ P5
 
-    P4_ = BSplineSpace{p4-1}(knots(P4)[2:end-1])
-    P5_ = BSplineSpace{p5-1}(knots(P5)[2:end-1])
-    @test P4_ ⊑ P5_
-
     n4, n5 = dim(P4), dim(P5)
     A45 = changebasis(P4, P5)
     @test size(A45) == (n4, n5)
     Δ45 = [bsplinebasis(P4, i, t) - sum(A45[i, j] * bsplinebasis(P5, j, t) for j in 1:n5) for i in 1:n1, t in 2 * rand(10) .+ 2]
     @test norm(Δ45) < ε
+
+    P6 = BSplineSpace{p4-1}(knots(P4)[2:end-1])
+    P7 = BSplineSpace{p5-1}(knots(P5)[2:end-1])
+    @test P6 ⊑ P7
+    @test P6 ⊈ P7
+
+    n6, n7 = dim(P6), dim(P7)
+    A67 = changebasis(P6, P7)
+    @test size(A67) == (n6, n7)
+    Δ67 = [bsplinebasis(P6, i, t) - sum(A67[i, j] * bsplinebasis(P7, j, t) for j in 1:n7) for i in 1:n1, t in 2 * rand(10) .+ 2]
+    @test norm(Δ67) < ε
 end
