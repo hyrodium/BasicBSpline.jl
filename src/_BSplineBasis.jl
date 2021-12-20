f93386b4df33bda0fa3064bd6f09c960c72b9b2e
hyrodium <hyrodium@gmail.com>
2021-12-20 18:22:43 +0900

    make bsplinebasis₋₀ generated

--- src/_BSplineBasis.jl
+++ src/_BSplineBasis.jl
@@ -126,17 +126,29 @@ end
 ``i``-th B-spline basis function.
 Left-sided limit version.
 """
-function bsplinebasis₋₀(P::BSplineSpace{p}, i::Integer, t::Real)::Float64 where p
-    k = P.knots
-
-    if p == 0
-        return k[i] < t ≤ k[i+1]
-    else
-        return (
-            ((k[i+p] - k[i] ≠ 0) ? bsplinebasis₋₀(BSplineSpace{p-1}(k), i, t) * (t - k[i]) / (k[i+p] - k[i]) : 0) +
-            ((k[i+p+1] - k[i+1] ≠ 0) ? bsplinebasis₋₀(BSplineSpace{p-1}(k), i+1, t) * (k[i+p+1] - t) / (k[i+p+1] - k[i+1]) : 0)
-        )
+@generated function bsplinebasis₋₀(P::BSplineSpace{p,T}, i::Integer, t::Real) where {p, T}
+    ks = [Symbol(:k,i) for i in 1:p+2]
+    Ks = [Symbol(:K,i) for i in 1:p+1]
+    Bs = [Symbol(:B,i) for i in 1:p+1]
+    k_l = Expr(:tuple, ks...)
+    k_r = Expr(:tuple, :(v[i]), (:(v[i+$j]) for j in 1:p+1)...)
+    K_l(n) = Expr(:tuple, Ks[1:n]...)
+    B_l(n) = Expr(:tuple, Bs[1:n]...)
+    A_r(n) = Expr(:tuple, [:(T($(ks[i])<t≤$(ks[i+1]))) for i in 1:n]...)
+    K_r(m,n) = Expr(:tuple, [:(_d(t-$(ks[i]),$(ks[i+m])-$(ks[i]))) for i in 1:n]...)
+    B_r(n) = Expr(:tuple, [:($(Ks[i])*$(Bs[i])+(1-$(Ks[i+1]))*$(Bs[i+1])) for i in 1:n]...)
+    exs = Expr[]
+    for i in 1:p
+        push!(exs, :($(K_l(p+2-i)) = $(K_r(i,p+2-i))))
+        push!(exs, :($(B_l(p+1-i)) = $(B_r(p+1-i))))
     end
+    Expr(:block,
+        :(v = knots(P).vector),
+        :($k_l = $k_r),
+        :($(B_l(p+1)) = $(A_r(p+1))),
+        exs...,
+        :(return B1)
+    )
 end
 
 """
