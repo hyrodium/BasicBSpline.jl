a8cd9807dbbf32d25115f0badef8bf53b1d3764b
hyrodium <hyrodium@gmail.com>
2021-12-21 19:23:21 +0900

    add new BSplineManifold struct

--- src/_BSplineManifold.jl
+++ src/_BSplineManifold.jl
@@ -1,6 +1,26 @@
 # B-spline manifold
 
+# TODO add more type parameter Deg, T
 abstract type AbstractBSplineManifold{Dim} end
+
+# struct BSplineManifold{Dim,Deg,T,S<:Tuple,Dim₊₁} <: AbstractBSplineManifold{Dim,Deg,T}
+struct BSplineManifold{Dim,Deg,T,S<:Tuple,Dim₊₁} <: AbstractBSplineManifold{Dim}
+    bsplinespaces::S
+    controlpoints::Array{T,Dim₊₁}
+    function BSplineManifold(Ps::S,a::Array{T,Dim₊₁}) where {S<:Tuple,Dim₊₁,T<:Real}
+        if !all(isa.(Ps,AbstractBSplineSpace))
+            # TODO: update error message
+            error("invalid")
+        end
+        if size(a)[1:Dim₊₁-1] != dim.(Ps)
+            # TODO: update error message
+            error("invalid")
+        end
+        d = length(Ps)
+        p = degree.(Ps)
+        new{d,p,T,S,d+1}(Ps,a)
+    end
+end
 
 function ⊗(X::AbstractArray{<:Real}, Y::AbstractArray{<:Real})
     # TODO: remove this function and use Tensor.jl
