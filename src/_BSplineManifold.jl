42f0b89ac97956aad27eae3d789c4dfaa47c42ef
hyrodium <hyrodium@gmail.com>
2021-12-22 02:20:21 +0900

    add 3-dimesional B-spline manifold mapping

--- src/_BSplineManifold.jl
+++ src/_BSplineManifold.jl
@@ -62,3 +62,22 @@ end
         :(return v)
     )
 end
+
+@generated function (M::BSplineManifold{3,Deg,S,T})(t1,t2,t3) where {Deg,S,T}
+    p1, p2, p3 = Deg
+    exs = Expr[]
+    for j3 in 1:p3+1, j2 in 1:p2+1, j1 in 1:p1+1
+        push!(exs, :(v .+= b1[$(j1)]*b2[$(j2)]*b3[$(j3)]*view(a,i1+$(j1-1),i2+$(j2-1),i3+$(j3-1),:)))
+    end
+    deleteat!(exs,1)
+    Expr(
+        :block,
+        :((P1, P2, P3) = bsplinespaces(M)),
+        :(a = controlpoints(M)),
+        :((i1, i2, i3) = (intervalindex(P1,t1), intervalindex(P2,t2), intervalindex(P3,t3))),
+        :((b1, b2, b3) = (bsplinebasisall(P1,i1,t1), bsplinebasisall(P2,i2,t2), bsplinebasisall(P3,i3,t3))),
+        :(v = b1[1]*b2[1]*b3[1]*view(a,i1,i2,i3,:)),
+        exs...,
+        :(return v)
+    )
+end
