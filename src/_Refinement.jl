ae27ae2e7001a73f6bc788fa7d3d9a847b2266e6
hyrodium <hyrodium@gmail.com>
2021-12-21 23:10:27 +0900

    update refinement

--- src/_Refinement.jl
+++ src/_Refinement.jl
@@ -1,5 +1,23 @@
 # Refinement
 
+# TODO: general dimension
+
+@doc raw"""
+Refinement of B-spline manifold with given B-spline spaces.
+"""
+function refinement(M::BSplineManifold{1}, Ps′::Tuple{<:AbstractBSplineSpace})
+    Ps = bsplinespaces(M)
+    a = controlpoints(M)
+    n = dim.(Ps)
+    n′ = dim.(Ps′)
+    d = size(a,2)  # 2 == Dim+1
+
+    A = changebasis.(Ps, Ps′)
+    a′ = [sum(A[1][I₁, J₁] * a[I₁,i] for I₁ in 1:n[1]) for J₁ in 1:n′[1], i in 1:d]
+    return BSplineManifold(Ps′, a′)
+end
+
+
 @doc raw"""
 Refinement of B-spline manifold with given B-spline spaces.
 """
@@ -25,6 +43,31 @@ function refinement(M::AbstractBSplineManifold, Ps′::Vector{<:AbstractBSplineS
     return (typeof(M).name.wrapper)(Ps′, a′)
 end
 
+
+@doc raw"""
+Refinement of B-spline manifold with additional degree and knots.
+"""
+function refinement(M::BSplineManifold{Dim}; p₊::Union{Nothing,NTuple{Dim,Int}}=nothing, k₊::Union{Nothing,NTuple{Dim,Knots{T}}}=nothing) where {Dim, T}
+    Ps = bsplinespaces(M)
+    if isnothing(p₊)
+        p₊ = tuple(zeros(Int, Dim)...)
+    elseif length(Ps) ≠ length(p₊)
+        throw(DimensionMismatch())
+    end
+    if isnothing(k₊)
+        k₊ = tuple(zeros(Knots, Dim)...)
+    elseif length(Ps) ≠ length(k₊)
+        throw(DimensionMismatch())
+    end
+
+    Ps′ = [(P = Ps[i];
+    p = degree(P);
+    k = knots(P);
+    k_unique = unique(k[1+p:end-p]);
+    BSplineSpace{p + p₊[i]}(k + p₊[i] * k_unique + k₊[i])) for i in eachindex(Ps)]
+
+    return refinement(M, tuple(Ps′...))
+end
 
 @doc raw"""
 Refinement of B-spline manifold with additional degree and knots.
