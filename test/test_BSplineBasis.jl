29460aae014fa8e1902f927908e8d4d56dbf6f30
hyrodium <hyrodium@gmail.com>
2021-12-26 20:10:44 +0900

    remove derivative tests from test_BSplineBasis.jl

--- test/test_BSplineBasis.jl
+++ test/test_BSplineBasis.jl
@@ -40,21 +40,6 @@ end
         end
 
         # Check the values of the derivative of B-spline basis funcitons
-        for i in 1:n
-            for t in rand(5)
-                @test (bsplinebasis′₊₀(P, i, t)
-                     ≈ bsplinebasis′₋₀(P, i, t)
-                     ≈ bsplinebasis′(P, i, t))
-            end
-            for t in k
-                t₊₀ = nextfloat(t)
-                t₋₀ = prevfloat(t)
-                @test bsplinebasis′₊₀(P, i, t) ≒ bsplinebasis′₊₀(P, i, t₊₀)
-                @test bsplinebasis′₋₀(P, i, t) ≒ bsplinebasis′₋₀(P, i, t₋₀)
-            end
-        end
-
-        # Check the values of the derivative of B-spline basis funcitons
         for t in k
             s = sum([bsplinebasis(P, i, t) for i in 1:n])
             s₊₀ = sum([bsplinebasis₊₀(P, i, t) for i in 1:n])
@@ -73,20 +58,6 @@ end
                 @test s₋₀ ≈ 1
             end
         end
-
-        # Check the derivative
-        @testset "derivative" begin
-            Random.seed!(57)
-            for i in 1:n, t in rand(5)
-                b′₊₀ = bsplinebasis′₊₀(P, i, t)
-                b′₋₀ = bsplinebasis′₋₀(P, i, t)
-                b′   = bsplinebasis′(P, i, t)
-                a′₊₀ = (bsplinebasis₊₀(P, i, t+Δt) - bsplinebasis₊₀(P, i, t-Δt)) / 2Δt
-                a′₋₀ = (bsplinebasis₋₀(P, i, t+Δt) - bsplinebasis₋₀(P, i, t-Δt)) / 2Δt
-                a′   = (bsplinebasis(P, i, t+Δt) - bsplinebasis(P, i, t-Δt)) / 2Δt
-                @test b′₊₀ ≈ b′₋₀ ≈ b′ ≈ a′₊₀ ≈ a′₋₀ ≈ a′
-            end
-        end
     end
 
     @testset "_bsplinebasis" begin
@@ -101,7 +72,6 @@ end
                 j = intervalindex(P,t)
                 _B = bsplinebasisall(P,j,t)
                 B = [bsplinebasis(P,i,t) for i in j:j+p]
-                B′ = [bsplinebasis′(P,i,t) for i in j:j+p]
                 @test norm(collect(_B) - B) < ε
 
                 _B = [bsplinebasis(P,i,t) for i in j:j+p]
@@ -112,15 +82,6 @@ end
 
                 _B = [bsplinebasis₋₀(P,i,t) for i in j:j+p]
                 @test norm(_B - B) < ε
-
-                _B′ =[ bsplinebasis′(P,i,t) for i in j:j+p]
-                @test norm(_B′ - B′) < ε
-
-                _B′ =[ bsplinebasis′₊₀(P,i,t) for i in j:j+p]
-                @test norm(_B′ - B′) < ε
-
-                _B′ =[ bsplinebasis′₋₀(P,i,t) for i in j:j+p]
-                @test norm(_B′ - B′) < ε
             end
         end
     end
