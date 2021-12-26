826c0502fa44088455c708f54c41fb7574932898
hyrodium <hyrodium@gmail.com>
2021-12-26 17:32:52 +0900

    update tests for derivatives

--- test/test_Derivative.jl
+++ test/test_Derivative.jl
@@ -1,23 +1,27 @@
 @testset "Derivative" begin
-    p_max = 5
+    Random.seed!(42)
+    p_max = 4
     ts = rand(10)
     dt = 1e-7
 
-    for p in 1:p_max
+    # Not sure why this @testset doesn't work fine.
+    # @testset "$(p)-th degree basis" for p in 0:p_max
+    for p in 0:p_max
         k = Knots(rand(20)) + (p+1)*Knots(0,1)
         P = BSplineSpace{p}(k)
         P0 = BSplineDerivativeSpace{0}(P)
         for t in ts, i in 1:dim(P)
             @test bsplinebasis(P,i,t) == bsplinebasis(P0,i,t)
         end
-        
+
         for r in 1:p_max
             P1 = BSplineDerivativeSpace{r-1}(P)
             P2 = BSplineDerivativeSpace{r}(P)        
+            @test degree(P2) == p-r
             for t in ts, i in 1:dim(P)
-                d1 = (bsplinebasis(P1,6,t+dt) - bsplinebasis(P1,6,t-dt))/2dt
-                d2 = bsplinebasis(P2,6,t)
-                @test d1 ≈ d2 atol=1e-4
+                d1 = (bsplinebasis(P1,i,t+dt) - bsplinebasis(P1,i,t-dt))/2dt
+                d2 = bsplinebasis(P2,i,t)
+                @test d1 ≈ d2
             end
         end
     end
