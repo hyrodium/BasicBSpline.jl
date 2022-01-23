a0c6a3974ed44e1c3d054bc3a6464a91c3f4aff8
hyrodium <hyrodium@gmail.com>
2022-01-23 20:23:33 +0900

    update mapping such as M(t1,t2)

--- src/_BSplineManifold.jl
+++ src/_BSplineManifold.jl
@@ -83,12 +83,25 @@ end
     )
 end
 
-function (M::AbstractBSplineManifold{Dim})(ts::Real...) where Dim
+@inline function (M::AbstractBSplineManifold{1})(t1)
     Ps = bsplinespaces(M)
-    for i in 1:Dim
-        ts[i] in domain(Ps[i]) || throw(DomainError(ts[i], "The input is out of range."))
-    end
-    _mapping(M,ts...)
+    t1 in domain(Ps[1]) || throw(DomainError(t1, "The input $(t1) is out of range."))
+    _mapping(M,t1)
+end
+
+@inline function (M::AbstractBSplineManifold{2})(t1,t2)
+    Ps = bsplinespaces(M)
+    t1 in domain(Ps[1]) || throw(DomainError(t1, "The input $(t1) is out of range."))
+    t2 in domain(Ps[2]) || throw(DomainError(t2, "The input $(t2) is out of range."))
+    _mapping(M,t1,t2)
+end
+
+@inline function (M::AbstractBSplineManifold{3})(t1,t2,t3)
+    Ps = bsplinespaces(M)
+    t1 in domain(Ps[1]) || throw(DomainError(t1, "The input $(t1) is out of range."))
+    t2 in domain(Ps[2]) || throw(DomainError(t2, "The input $(t2) is out of range."))
+    t3 in domain(Ps[3]) || throw(DomainError(t3, "The input $(t3) is out of range."))
+    _mapping(M,t1,t2,t3)
 end
 
 # TODO add mappings higher dimensionnal B-spline manifold with @generated macro
