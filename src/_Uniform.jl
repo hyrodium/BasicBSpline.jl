ef520e3bfe6119130b14a0b67a5939a05f82f506
hyrodium <hyrodium@gmail.com>
2022-01-24 22:42:50 +0900

    add uniform_bsplinebasisall_kernel

--- src/_Uniform.jl
+++ src/_Uniform.jl
@@ -23,3 +23,28 @@ end
 BasicBSpline.knotvector(P::UniformBSplineSpace) = P.knotvector
 
 _lower(P::UniformBSplineSpace{p,T}) where {p,T} = UniformBSplineSpace{p-1}(knotvector(P))
+
+@inline function uniform_bsplinebasisall_kernel(::Val{0},t)
+    SVector{1}(one(t))
+end
+@inline function uniform_bsplinebasisall_kernel(::Val{1},t)
+    SVector{2}(1-t,t)
+end
+@generated function uniform_bsplinebasisall_kernel(::Val{p}, t) where p
+    bs = [Symbol(:b,i) for i in 1:p]
+    Bs = [Symbol(:B,i) for i in 1:p+1]
+    K1s = [:(($(j)-t)/$(p)) for j in 1:p]
+    K2s = [:((t-$(j-p))/$(p)) for j in 1:p]
+    b = Expr(:tuple, bs...)
+    B = Expr(:tuple, Bs...)
+    exs = [:($(Bs[j+1]) = ($(K1s[j+1])*$(bs[j+1]) + $(K2s[j])*$(bs[j]))) for j in 1:p-1]
+    Expr(
+        :block,
+        :($(Expr(:meta, :inline))),
+        :($b = uniform_bsplinebasisall_kernel(Val{$(p-1)}(),t)),
+        :($(Bs[1]) = $(K1s[1])*$(bs[1])),
+        exs...,
+        :($(Bs[p+1]) = $(K2s[p])*$(bs[p])),
+        :(return SVector($(B)))
+    )
+end
