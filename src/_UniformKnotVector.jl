1bb9ed0c9448a55257257b85429a04033b682283
hyrodium <hyrodium@gmail.com>
2022-01-26 22:41:41 +0900

    update UniformKnotVector constructor, conversion, etc.

--- src/_UniformKnotVector.jl
+++ src/_UniformKnotVector.jl
@@ -1,10 +1,11 @@
 struct UniformKnotVector{T,R<:AbstractRange} <: AbstractKnotVector{T}
     vector::R
-    global unsafe_uniformknotvector(v::R) where R<:AbstractRange{T} where T = new{T,R}(v)
+    global unsafe_uniformknotvector(::Type{T}, v::R) where R<:AbstractRange{T} where T = new{T,R}(v)
 end
-UniformKnotVector(v::AbstractRange) = unsafe_uniformknotvector(sort(v))
+UniformKnotVector(v::AbstractRange{T}) where T = unsafe_uniformknotvector(T,sort(v))
 UniformKnotVector(k::UniformKnotVector) = k
-UniformKnotVector{T,R}(k::UniformKnotVector) where R<:AbstractRange{T} where T = UniformKnotVector(R(k.vector))
+UniformKnotVector{T}(k::UniformKnotVector) where T = unsafe_uniformknotvector(T, k.vector)
+UniformKnotVector{T,R}(k::UniformKnotVector) where R<:AbstractRange{T} where T = unsafe_uniformknotvector(T, R(k.vector))
 
 _vec(k::UniformKnotVector) = k.vector
 
@@ -23,6 +24,10 @@ function Base.convert(::Type{KnotVector{T}},k::UniformKnotVector) where T
     unsafe_knotvector(T,k.vector)
 end
 
+function Base.convert(::Type{UniformKnotVector{T,R}},k::UniformKnotVector) where {T,R}
+    UniformKnotVector{T,R}(k)
+end
+
 function Base.promote_rule(::Type{KnotVector{T}}, ::Type{UniformKnotVector{S,R}}) where {T,S,R}
     KnotVector{promote_type(T,S)}
 end
@@ -34,10 +39,11 @@ Base.searchsortedfirst(k::UniformKnotVector,t) = searchsortedfirst(k.vector,t)
 Base.searchsortedlast(k::UniformKnotVector,t) = searchsortedlast(k.vector,t)
 Base.searchsorted(k::UniformKnotVector,t) = searchsorted(k.vector,t)
 
-# TODO: remove collect
 Base.collect(k::UniformKnotVector) = collect(k.vector)
 
 Base.:+(k1::UniformKnotVector{T1},k2::UniformKnotVector{T2}) where {T1,T2} = KnotVector{promote_type(T1,T2)}([k1.vector;k2.vector])
+
+Base.zero(::UniformKnotVector{T}) where T = KnotVector{T}()
 
 function 𝔫(k::UniformKnotVector, t::Real)
     return Int(t ∈ k)
