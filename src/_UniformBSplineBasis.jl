171e1ad59c86b61d15f47b1dfea52d8d63b50cf5
hyrodium <hyrodium@gmail.com>
2022-01-30 01:05:30 +0900

    update bsplinebasis functions for UniformBSplineSpace

--- src/_UniformBSplineBasis.jl
+++ src/_UniformBSplineBasis.jl
@@ -1,12 +1,15 @@
 # Uniform BSpline Basis
 
-@inline function uniform_bsplinebasisall_kernel(::Val{0},t)
-    SVector{1}(one(t))
+## Kernel funcitons of uniform B-spline basis function
+@inline function uniform_bsplinebasisall_kernel(::Val{0},t::T) where T<:Real
+    U = StaticArrays.arithmetic_closure(T)
+    SVector{1,U}(one(t))
 end
-@inline function uniform_bsplinebasisall_kernel(::Val{1},t)
-    SVector{2}(1-t,t)
+@inline function uniform_bsplinebasisall_kernel(::Val{1},t::T) where T<:Real
+    U = StaticArrays.arithmetic_closure(T)
+    SVector{2,U}(1-t,t)
 end
-@generated function uniform_bsplinebasisall_kernel(::Val{p}, t) where p
+@generated function uniform_bsplinebasisall_kernel(::Val{p}, t::T) where {p, T<:Real}
     bs = [Symbol(:b,i) for i in 1:p]
     Bs = [Symbol(:B,i) for i in 1:p+1]
     K1s = [:(($(j)-t)/$(p)) for j in 1:p]
@@ -26,11 +29,37 @@ end
 end
 
 @inline function uniform_bsplinebasis_kernel(::Val{0},t::T) where T<:Real
-    return zero(t) ≤ t < one(t)
+    U = StaticArrays.arithmetic_closure(T)
+    return U(zero(t) ≤ t < one(t))
 end
 @inline function uniform_bsplinebasis_kernel(::Val{1},t::T) where T<:Real
-    return max(1-abs(t-1), zero(t))
+    U = StaticArrays.arithmetic_closure(T)
+    return U(max(1-abs(t-1), zero(t)))
 end
 @inline function uniform_bsplinebasis_kernel(::Val{p},t::T) where {p,T<:Real}
     return (t*uniform_bsplinebasis_kernel(Val{p-1}(),t) + (p+1-t)*uniform_bsplinebasis_kernel(Val{p-1}(),t-1))/p
 end
+
+## bsplinebasis
+@inline function bsplinebasis(P::UniformBSplineSpace{p,T,R},i::Integer,t::S) where {p, T, R<:AbstractUnitRange, S<:Real}
+    U = StaticArrays.arithmetic_closure(promote_type(T,S))
+    k = knotvector(P)
+    @boundscheck (1 ≤ i) || throw(BoundsError(knotvector(P), i))
+    @boundscheck (i ≤ dim(P)) || throw(BoundsError(knotvector(P), i+p+1))
+    return U(uniform_bsplinebasis_kernel(Val{p}(),t-i+1-k[1]))
+end
+@inline function bsplinebasis(P::UniformBSplineSpace{p,T},i::Integer,t::S) where {p, T, S<:Real}
+    U = StaticArrays.arithmetic_closure(promote_type(T,S))
+    @boundscheck (1 ≤ i) || throw(BoundsError(knotvector(P), i))
+    @boundscheck (i ≤ dim(P)) || throw(BoundsError(knotvector(P), i+p+1))
+    k = knotvector(P)
+    a = @inbounds k.vector[i]
+    b = @inbounds k.vector[i+p+1]
+    return U(uniform_bsplinebasis_kernel(Val{p}(),(p+1)*(t-a)/(b-a)))
+end
+@inline bsplinebasis₋₀(P::UniformBSplineSpace,i::Integer,t::Real) = bsplinebasis(P,i,t)
+@inline bsplinebasis₊₀(P::UniformBSplineSpace,i::Integer,t::Real) = bsplinebasis(P,i,t)
+
+## bsplinebasisall
+# @inline function bsplinebasisall(P::UniformBSplineSpace{p},i::Integer,t::Real) where p
+# end
