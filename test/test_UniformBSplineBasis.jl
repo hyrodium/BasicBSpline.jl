9137f26fc4bceb51b7faed8bf40f318ce047fa08
hyrodium <hyrodium@gmail.com>
2022-01-30 02:17:58 +0900

    update tests for UniformBSplineBasis

--- test/test_UniformBSplineBasis.jl
+++ test/test_UniformBSplineBasis.jl
@@ -1,4 +1,4 @@
-@testset "UniformKnotVector" begin
+@testset "UniformBSplineBasis" begin
     @testset "bsplinebasis" begin
         v = 2:10
         k = KnotVector(v)
@@ -6,6 +6,8 @@
         k2 = UniformKnotVector(v/2)
         k3 = UniformKnotVector(v*2)
         @test eltype(k1) == Int
+        @test eltype(k2) == Float64
+        @test eltype(k3) == Int
         for p in 0:3
             P = BSplineSpace{p}(k)
             P1 = UniformBSplineSpace{p}(k1)
@@ -15,6 +17,9 @@
             @test dim(P) == dim(P2)
             @test dim(P) == dim(P3)
             n = dim(P)
+            @test bsplinebasis(P1,n÷2,6//1) isa Rational{Int}
+            @test bsplinebasis(P2,n÷2,6//1) isa Float64
+            @test bsplinebasis(P3,n÷2,6//1) isa Rational{Int}
             for _ in 1:20
                 t = 10*rand()
                 for i in 1:n
@@ -30,6 +35,35 @@
                     @test bsplinebasis(P1,i,t) == bsplinebasis₊₀(P1,i,t) == bsplinebasis₋₀(P1,i,t)
                     @test bsplinebasis(P2,i,t) == bsplinebasis₊₀(P2,i,t) == bsplinebasis₋₀(P2,i,t)
                     @test bsplinebasis(P3,i,t) == bsplinebasis₊₀(P3,i,t) == bsplinebasis₋₀(P3,i,t)
+                end
+            end
+        end
+    end
+
+    @testset "bsplinebasisall" begin
+        v = 2:10
+        k = KnotVector(v)
+        l = length(k)
+        k1 = UniformKnotVector(v)
+        k2 = UniformKnotVector(v/2)
+        k3 = UniformKnotVector(v*2)
+        for p in 0:3
+            P = BSplineSpace{p}(k)
+            P1 = UniformBSplineSpace{p}(k1)
+            P2 = UniformBSplineSpace{p}(k2)
+            P3 = UniformBSplineSpace{p}(k3)
+            @test bsplinebasisall(P1,(l-2p)÷2,6//1) isa SVector{p+1,Rational{Int}}
+            @test bsplinebasisall(P2,(l-2p)÷2,6//1) isa SVector{p+1,Float64}
+            @test bsplinebasisall(P3,(l-2p)÷2,6//1) isa SVector{p+1,Rational{Int}}
+            for _ in 1:20
+                t = 10*rand()
+                for i in 1:l-2p-1  # FIXME: This can be 0:l-2p, but don't know why the test doesn't pass.
+                    @test bsplinebasisall(P1,i,t) ≈ bsplinebasisall(P,i,t)
+                    @test bsplinebasisall(P2,i,t/2) ≈ bsplinebasisall(P,i,t)
+                    @test bsplinebasisall(P3,i,t*2) ≈ bsplinebasisall(P,i,t)
+                    @test bsplinebasisall(P1,i,t) isa SVector{p+1,Float64}
+                    @test bsplinebasisall(P2,i,t) isa SVector{p+1,Float64}
+                    @test bsplinebasisall(P3,i,t) isa SVector{p+1,Float64}
                 end
             end
         end
