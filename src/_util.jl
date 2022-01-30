a5bb23de584cb15ad973169623e3ae4654633db7
hyrodium <hyrodium@gmail.com>
2022-01-30 13:11:20 +0900

    remove _inf, replace ifelse with && in _d

--- src/_util.jl
+++ src/_util.jl
@@ -1,10 +1,7 @@
 # Division, devided by zero will be zero
 @inline function _d(a::T,b::T) where T
     U=StaticArrays.arithmetic_closure(T)
-    return ifelse(iszero(b), zero(U), U(a/b))
+    iszero(b) && return zero(U)
+    return U(a/b)
 end
 @inline _d(a,b) = _d(promote(a,b)...)
-
-# Create ininity from given type
-@inline _inf(::Type{T}) where T<:Number = one(T)/zero(T)
-@inline _inf(::T) where T<:Number = _inf(T)
