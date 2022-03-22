37af51a45a5dfc450e65ed9a90e89946ae5eb163
hyrodium <hyrodium@gmail.com>
2022-03-22 23:37:39 +0900

    remove inv

--- src/_util.jl
+++ src/_util.jl
@@ -8,3 +8,8 @@ end
 
 # Euler's triangle number (http://oeis.org/wiki/Eulerian_numbers,_triangle_of)
 eulertriangle(n,k) = sum((-1)^j*binomial(n+1,j)*(k-j+1)^n for j in 0:k)
+
+# Left division function
+_leftdivision(A,b) = inv(A)*b
+_leftdivision(A,b::Number) = A\b
+# TODO: add more methods for left division (e.g. b::Vector{<:SVector})
