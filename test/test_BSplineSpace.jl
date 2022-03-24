9b456cf833463ae5eff6e2ab29d466d8c617ad3e
hyrodium <hyrodium@gmail.com>
2022-03-24 22:25:37 +0900

    remove test_broken

--- test/test_BSplineSpace.jl
+++ test/test_BSplineSpace.jl
@@ -100,10 +100,15 @@
         @test P3 ⊆ expandspace_R(P3, p₊=1)
         @test P4 ⊆ expandspace_R(P4, p₊=1)
 
-        @test_broken P1 ⊑ expandspace_I(P1, p₊=1) == expandspace(P1, p₊=1)
+        # P1 and P4 is not subset of expandspace_I.
+        @test P1 ⋢ expandspace_I(P1, p₊=1) == expandspace(P1, p₊=1)
         @test P2 ⊑ expandspace_I(P2, p₊=1) == expandspace(P2, p₊=1)
         @test P3 ⊑ expandspace_I(P3, p₊=1) == expandspace(P3, p₊=1)
-        @test_broken P4 ⊑ expandspace_I(P4, p₊=1) == expandspace(P4, p₊=1)
+        @test P4 ⋢ expandspace_I(P4, p₊=1) == expandspace(P4, p₊=1)
+
+        # That was because P1 and P4 is not nondegenerate.
+        @test isdegenerate_I(P1)
+        @test isdegenerate_I(P4)
     end
 
     @testset "sqsubset and lowered B-spline space" begin
