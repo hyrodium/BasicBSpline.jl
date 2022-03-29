c3344c64b65afd936f1cd19cb4a9d3824b509d05
hyrodium <hyrodium@gmail.com>
2022-03-29 12:34:54 +0900

    add BSplineCurve plotting in 3d

--- src/_PlotRecipes.jl
+++ src/_PlotRecipes.jl
@@ -1,4 +1,4 @@
-@recipe function f(M::BSplineManifold{1,ps,<:StaticVector{2,<:Real}}) where ps
+@recipe function f(M::BSplineManifold{1,Deg,<:StaticVector{2,<:Real}}) where Deg
     @series begin
         primary := false
         linecolor := :lightgray
@@ -10,6 +10,20 @@
     t1,t2 = extrema(domain(bsplinespaces(M)[1]))
     p = M.(range(t1,t2,300))
     getindex.(p,1),getindex.(p,2)
+end
+
+@recipe function f(M::BSplineManifold{1,Deg,<:StaticVector{3,<:Real}}) where Deg
+    @series begin
+        primary := false
+        linecolor := :lightgray
+        markershape := :circle
+        markercolor := :lightgray
+        a = controlpoints(M)
+        getindex.(a,1),getindex.(a,2),getindex.(a,3)
+    end
+    t1,t2 = extrema(domain(bsplinespaces(M)[1]))
+    p = M.(range(t1,t2,300))
+    getindex.(p,1),getindex.(p,2),getindex.(p,3)
 end
 
 #=
