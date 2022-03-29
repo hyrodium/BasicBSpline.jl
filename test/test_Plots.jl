4934e8cf3de9e9149107849302508e74d2b6bb12
hyrodium <hyrodium@gmail.com>
2022-03-29 19:50:32 +0900

    add tests for plotting rational B-spline curve

--- test/test_Plots.jl
+++ test/test_Plots.jl
@@ -1,19 +1,39 @@
 @testset "Plots" begin
-    @testset "curve in 2d" begin
-        pts = [SVector(0, 0), SVector(1, 1), SVector(2, -1), SVector(3, 0), SVector(4, -2), SVector(5, 1)]
+    @testset "B-spline curve in 2d" begin
+        a = [SVector(0, 0), SVector(1, 1), SVector(2, -1), SVector(3, 0), SVector(4, -2), SVector(5, 1)]
         p = 3
         k = KnotVector(0:3)+p*KnotVector(0,3)
         P = BSplineSpace{p}(k)
-        M = BSplineManifold(pts, (P,))
+        M = BSplineManifold(a, (P,))
         plot(M)
     end
 
-    @testset "curve in 3d" begin
-        pts = [SVector(0, 0, 2), SVector(1, 1, 1), SVector(2, -1, 4), SVector(3, 0, 0), SVector(4, -2, 0), SVector(5, 1, 2)]
+    @testset "B-spline curve in 3d" begin
+        a = [SVector(0, 0, 2), SVector(1, 1, 1), SVector(2, -1, 4), SVector(3, 0, 0), SVector(4, -2, 0), SVector(5, 1, 2)]
         p = 3
         k = KnotVector(0:3)+p*KnotVector(0,3)
         P = BSplineSpace{p}(k)
-        M = BSplineManifold(pts, (P,))
+        M = BSplineManifold(a, (P,))
+        plot(M)
+    end
+
+    @testset "Rational B-spline curve in 2d" begin
+        a = [SVector(0, 0), SVector(1, 1), SVector(2, -1), SVector(3, 0), SVector(4, -2), SVector(5, 1)]
+        w = rand(6)
+        p = 3
+        k = KnotVector(0:3)+p*KnotVector(0,3)
+        P = BSplineSpace{p}(k)
+        M = RationalBSplineManifold(a, w, (P,))
+        plot(M)
+    end
+
+    @testset "Rational B-spline curve in 3d" begin
+        a = [SVector(0, 0, 2), SVector(1, 1, 1), SVector(2, -1, 4), SVector(3, 0, 0), SVector(4, -2, 0), SVector(5, 1, 2)]
+        w = rand(6)
+        p = 3
+        k = KnotVector(0:3)+p*KnotVector(0,3)
+        P = BSplineSpace{p}(k)
+        M = RationalBSplineManifold(a, w, (P,))
         plot(M)
     end
 end
