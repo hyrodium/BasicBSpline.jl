f81739a9938e181a3ded320cb7e4d7d283b09052
hyrodium <hyrodium@gmail.com>
2022-04-03 23:24:40 +0900

    add tests for plotting B-spline space

--- test/test_Plots.jl
+++ test/test_Plots.jl
@@ -1,4 +1,18 @@
 @testset "Plots" begin
+    @testset "B-spline space" begin
+        p = 3
+        k = KnotVector(0:3)+p*KnotVector(0,3)
+        P = BSplineSpace{p}(k)
+        plot(P)
+    end
+
+    @testset "uniform B-spline space" begin
+        p = 3
+        k = UniformKnotVector(0:8)
+        P = UniformBSplineSpace{p}(k)
+        plot(P)
+    end
+
     @testset "B-spline curve in 2d" begin
         a = [SVector(0, 0), SVector(1, 1), SVector(2, -1), SVector(3, 0), SVector(4, -2), SVector(5, 1)]
         p = 3
