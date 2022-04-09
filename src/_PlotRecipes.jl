f9e570d070722da8e66b4cf8c65a91379a206b52
Yuto Horikawa <hyrodium@gmail.com>
2022-04-10 02:18:23 +0900

    Add plotting recipes for B-spline surface (#199)
    * update plotting recipes for B-spline curves
    
    * add plotting recipe for B-spline surface
    
    * add tests for plotting B-spline surface
    
    * update controlpoints color
    
    * minor updates

--- src/_PlotRecipes.jl
+++ src/_PlotRecipes.jl
@@ -1,7 +1,7 @@
 # B-spline space
 @recipe function f(P::AbstractBSplineSpace{p}) where p
     # TODO fix number of sampling points
-    N = 100(p+1)
+    N = 100
     k = knotvector(P)
     ts = Float64[]
     for i in 1:length(k)-1
@@ -15,16 +15,16 @@
     tt, bb
 end
 
-# B-spline Curve
-@recipe function f(M::BSplineManifold{1,Deg,<:StaticVector{2,<:Real}}) where Deg
+# B-spline curve in 2d
+@recipe function f(M::Union{BSplineManifold{1,Deg,<:StaticVector{2,<:Real}}, RationalBSplineManifold{1,Deg,<:StaticVector{2,<:Real}}}) where Deg
     # TODO fix number of sampling points
     t_min, t_max = extrema(domain(bsplinespaces(M)[1]))
     ts = range(t_min, t_max, length=300)
     @series begin
         primary := false
-        linecolor := :lightgray
+        linecolor := :gray
         markershape := :circle
-        markercolor := :lightgray
+        markercolor := :gray
         a = controlpoints(M)
         getindex.(a,1), getindex.(a,2)
     end
@@ -32,15 +32,16 @@ end
     getindex.(p,1), getindex.(p,2)
 end
 
-@recipe function f(M::BSplineManifold{1,Deg,<:StaticVector{3,<:Real}}) where Deg
+# B-spline curve in 3d
+@recipe function f(M::Union{BSplineManifold{1,Deg,<:StaticVector{3,<:Real}}, RationalBSplineManifold{1,Deg,<:StaticVector{3,<:Real}}}) where Deg
     # TODO fix number of sampling points
     t_min, t_max = extrema(domain(bsplinespaces(M)[1]))
     ts = range(t_min, t_max, length=300)
     @series begin
         primary := false
-        linecolor := :lightgray
+        linecolor := :gray
         markershape := :circle
-        markercolor := :lightgray
+        markercolor := :gray
         a = controlpoints(M)
         getindex.(a,1), getindex.(a,2), getindex.(a,3)
     end
@@ -48,42 +49,49 @@ end
     getindex.(p,1), getindex.(p,2), getindex.(p,3)
 end
 
-# Rational B-spline Curve
-@recipe function f(M::RationalBSplineManifold{1,Deg,<:StaticVector{2,<:Real}}) where Deg
+# B-spline surface
+@recipe function f(M::Union{BSplineManifold{2,Deg,<:StaticVector{3,<:Real}}, RationalBSplineManifold{2,Deg,<:StaticVector{3,<:Real}}}) where Deg
     # TODO fix number of sampling points
-    t_min, t_max = extrema(domain(bsplinespaces(M)[1]))
-    ts = range(t_min, t_max, length=300)
+    t1_min, t1_max = extrema(domain(bsplinespaces(M)[1]))
+    t2_min, t2_max = extrema(domain(bsplinespaces(M)[2]))
+    t1s = range(t1_min, t1_max, length=300)
+    t2s = range(t2_min, t2_max, length=300)
     @series begin
         primary := false
-        linecolor := :lightgray
+        linecolor := :gray
         markershape := :circle
-        markercolor := :lightgray
-        a = controlpoints(M)
-        getindex.(a,1), getindex.(a,2)
-    end
-    p = M.(ts)
-    getindex.(p,1), getindex.(p,2)
-end
-
-@recipe function f(M::RationalBSplineManifold{1,Deg,<:StaticVector{3,<:Real}}) where Deg
-    # TODO fix number of sampling points
-    t_min, t_max = extrema(domain(bsplinespaces(M)[1]))
-    ts = range(t_min, t_max, length=300)
-    @series begin
-        primary := false
-        linecolor := :lightgray
-        markershape := :circle
-        markercolor := :lightgray
+        markercolor := :gray
+        seriestype := :scatter
         a = controlpoints(M)
         getindex.(a,1), getindex.(a,2), getindex.(a,3)
     end
-    p = M.(ts)
-    getindex.(p,1), getindex.(p,2), getindex.(p,3)
+    @series begin
+        primary := false
+        linecolor := :gray
+        markershape := :circle
+        markercolor := :gray
+        seriestype := :path
+        a = controlpoints(M)
+        getindex.(a,1), getindex.(a,2), getindex.(a,3)
+    end
+    @series begin
+        primary := false
+        linecolor := :gray
+        markershape := :circle
+        markercolor := :gray
+        seriestype := :path
+        a = controlpoints(M)
+        getindex.(a',1), getindex.(a',2), getindex.(a',3)
+    end
+    ps = M.(t1s,t2s')
+    xs = getindex.(ps,1)
+    ys = getindex.(ps,2)
+    zs = getindex.(ps,3)
+    seriestype := :surface
+    xs,ys,zs
 end
 
 #=
 TODO
-* BSplineSurface
 * BSplineSolid
-* RationalBSplineManifold
 =#
