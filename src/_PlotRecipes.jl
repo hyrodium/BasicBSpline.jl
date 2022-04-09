834ca3c6af0cefc81e4104dd49115a376d50cd94
hyrodium <hyrodium@gmail.com>
2022-04-10 01:54:09 +0900

    add plotting recipe for B-spline surface

--- src/_PlotRecipes.jl
+++ src/_PlotRecipes.jl
@@ -49,6 +49,48 @@ end
     getindex.(p,1), getindex.(p,2), getindex.(p,3)
 end
 
+# B-spline surface
+@recipe function f(M::Union{BSplineManifold{2,Deg,<:StaticVector{3,<:Real}}, RationalBSplineManifold{2,Deg,<:StaticVector{3,<:Real}}}) where Deg
+    # TODO fix number of sampling points
+    u1_min, u1_max = extrema(domain(bsplinespaces(M)[1]))
+    u2_min, u2_max = extrema(domain(bsplinespaces(M)[2]))
+    u1s = range(u1_min, u1_max, length=300)
+    u2s = range(u2_min, u2_max, length=300)
+    @series begin
+        primary := false
+        linecolor := :lightgray
+        markershape := :circle
+        markercolor := :lightgray
+        seriestype := :scatter
+        a = controlpoints(M)
+        getindex.(a,1), getindex.(a,2), getindex.(a,3)
+    end
+    @series begin
+        primary := false
+        linecolor := :gray
+        markershape := :circle
+        markercolor := :lightgray
+        seriestype := :path
+        a = controlpoints(M)
+        getindex.(a,1), getindex.(a,2), getindex.(a,3)
+    end
+    @series begin
+        primary := false
+        linecolor := :gray
+        markershape := :circle
+        markercolor := :lightgray
+        seriestype := :path
+        a = controlpoints(M)
+        getindex.(a',1), getindex.(a',2), getindex.(a',3)
+    end
+    ps = M.(u1s,u2s')
+    xs = getindex.(ps,1)
+    ys = getindex.(ps,2)
+    zs = getindex.(ps,3)
+    seriestype := :surface
+    xs,ys,zs
+end
+
 #=
 TODO
 * BSplineSurface
