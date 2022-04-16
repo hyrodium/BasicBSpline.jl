370fa4aaf587abd033cf3bf069321f6508e245b0
hyrodium <hyrodium@gmail.com>
2022-04-17 01:10:50 +0900

    add docstring for bsplinesupport

--- src/_BSplineSpace.jl
+++ src/_BSplineSpace.jl
@@ -203,6 +203,21 @@ Return the support of ``i``-th B-spline basis function.
 ```math
 \operatorname{supp}(B_{(i,p,k)})=[k_{i},k_{i+p+1}]
 ```
+
+# Examples
+```jldoctest
+julia> k = KnotVector([0.0, 1.5, 2.5, 5.5, 8.0, 9.0, 9.5, 10.0])
+KnotVector([0.0, 1.5, 2.5, 5.5, 8.0, 9.0, 9.5, 10.0])
+
+julia> P = BSplineSpace{2}(k)
+BSplineSpace{2, Float64}(KnotVector([0.0, 1.5, 2.5, 5.5, 8.0, 9.0, 9.5, 10.0]))
+
+julia> bsplinesupport(P,1)
+0.0..5.5
+
+julia> bsplinesupport(P,2)
+1.5..8.0
+```
 """
 bsplinesupport(P::AbstractBSplineSpace, i::Integer) = bsplinesupport_R(P,i)
 
