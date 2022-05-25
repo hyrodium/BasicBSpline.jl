28171db4f5d10710584ca75386c0123115c61dbb
hyrodium <hyrodium@gmail.com>
2022-05-25 21:38:56 +0900

    add currying for 1dim B-spline manifold

--- src/_BSplineManifold.jl
+++ src/_BSplineManifold.jl
@@ -111,25 +111,32 @@ end
     )
 end
 
-@inline function (M::AbstractManifold{1})(t1)
+@inline function (M::AbstractManifold{1})(t1::Real)
     Ps = bsplinespaces(M)
     t1 in domain(Ps[1]) || throw(DomainError(t1, "The input $(t1) is out of range."))
     unsafe_mapping(M,t1)
 end
 
-@inline function (M::AbstractManifold{2})(t1,t2)
+@inline function (M::AbstractManifold{2})(t1::Real,t2::Real)
     Ps = bsplinespaces(M)
     t1 in domain(Ps[1]) || throw(DomainError(t1, "The input $(t1) is out of range."))
     t2 in domain(Ps[2]) || throw(DomainError(t2, "The input $(t2) is out of range."))
     unsafe_mapping(M,t1,t2)
 end
 
-@inline function (M::AbstractManifold{3})(t1,t2,t3)
+@inline function (M::AbstractManifold{3})(t1::Real,t2::Real,t3::Real)
     Ps = bsplinespaces(M)
     t1 in domain(Ps[1]) || throw(DomainError(t1, "The input $(t1) is out of range."))
     t2 in domain(Ps[2]) || throw(DomainError(t2, "The input $(t2) is out of range."))
     t3 in domain(Ps[3]) || throw(DomainError(t3, "The input $(t3) is out of range."))
     unsafe_mapping(M,t1,t2,t3)
+end
+
+# currying
+@inline function (M::AbstractManifold{1})(::Colon)
+    a = copy(controlpoints(M))
+    Ps = bsplinespaces(M)
+    return BSplineManifold(a,Ps)
 end
 
 # TODO add mappings higher dimensionnal B-spline manifold with @generated macro
