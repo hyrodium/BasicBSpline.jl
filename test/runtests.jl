9fbb3aa6490f8ffc35adddd2ed15788ab06e8ca7
hyrodium <hyrodium@gmail.com>
2022-08-08 18:15:28 +0900

    update tests with Aqua.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,20 +6,21 @@ using Random
 using StaticArrays
 using GeometryBasics
 using Plots
+using Aqua
 
-@testset "BasicBSpline.jl" begin
-    include("test_util.jl")
-    include("test_KnotVector.jl")
-    include("test_BSplineSpace.jl")
-    include("test_BSplineBasis.jl")
-    include("test_UniformKnotVector.jl")
-    include("test_UniformBSplineSpace.jl")
-    include("test_UniformBSplineBasis.jl")
-    include("test_Derivative.jl")
-    include("test_ChangeBasis.jl")
-    include("test_BSplineManifold.jl")
-    include("test_RationalBSplineManifold.jl")
-    include("test_Refinement.jl")
-    include("test_Fitting.jl")
-    include("test_Plots.jl")
-end
+Aqua.test_all(BasicBSpline)
+
+include("test_util.jl")
+include("test_KnotVector.jl")
+include("test_BSplineSpace.jl")
+include("test_BSplineBasis.jl")
+include("test_UniformKnotVector.jl")
+include("test_UniformBSplineSpace.jl")
+include("test_UniformBSplineBasis.jl")
+include("test_Derivative.jl")
+include("test_ChangeBasis.jl")
+include("test_BSplineManifold.jl")
+include("test_RationalBSplineManifold.jl")
+include("test_Refinement.jl")
+include("test_Fitting.jl")
+include("test_Plots.jl")
