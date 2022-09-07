a7d43274f1d11e850c9141fcc95f326d529b4596
hyrodium <hyrodium@gmail.com>
2022-09-07 23:45:02 +0900

    add frule and rrule for bsplinebasis

--- src/_ChainRules.jl
+++ src/_ChainRules.jl
@@ -1,1 +1,19 @@
+function ChainRulesCore.frule((_, ΔP, Δi, Δt), ::typeof(bsplinebasis), P::BSplineSpace, i::Integer, t::Real)
+    B = bsplinebasis(P,i,t)
+    ∂B_∂P = @not_implemented "hoge"
+    # ∂B_∂i = NoTangent()
+    ∂B_∂t = bsplinebasis′(P,i,t)
+    return (B, ∂B_∂P*ΔP + ∂B_∂t*Δt)
+end
 
+function ChainRulesCore.rrule(::typeof(bsplinebasis), P::BSplineSpace, i::Integer, t::Real)
+    B = bsplinebasis(P,i,t)
+    # project_t = ProjectTo(t)  # Not sure we need this ProjectTo.
+    function bsplinebasis_pullback(ΔB)
+        P̄ = @not_implemented "fuga"
+        ī = NoTangent()
+        t̄ = bsplinebasis′(P,i,t) * ΔB
+        return (NoTangent(), P̄, ī, t̄)
+    end
+    return (B, bsplinebasis_pullback)
+end
