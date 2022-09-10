da7ff9b03c951c51005bba8f8367a23f10384785
hyrodium <hyrodium@gmail.com>
2022-09-10 13:18:02 +0900

    update src for method calls like `BSplineManifold(a, P, P)`

--- src/_BSplineManifold.jl
+++ src/_BSplineManifold.jl
@@ -25,7 +25,7 @@ julia> a = [SVector(1,0), SVector(1,1), SVector(0,1)]
  [1, 1]
  [0, 1]
 
-julia> M = BSplineManifold(a,(P,));
+julia> M = BSplineManifold(a, P);
 
 julia> M(0.4)
 2-element SVector{2, Float64} with indices SOneTo(2):
@@ -55,6 +55,8 @@ struct BSplineManifold{Dim,Deg,C,S<:Tuple} <: AbstractBSplineManifold{Dim,Deg}
     end
 end
 
+BSplineManifold(a::Array{C,Dim},Ps::Vararg{AbstractBSplineSpace, Dim}) where {C,Dim} = BSplineManifold(a,Ps)
+
 Base.:(==)(M1::AbstractBSplineManifold, M2::AbstractBSplineManifold) = (bsplinespaces(M1)==bsplinespaces(M2)) & (controlpoints(M1)==controlpoints(M2))
 
 bsplinespaces(M::BSplineManifold) = M.bsplinespaces
@@ -72,7 +74,7 @@ BSplineSpace{1, Int64}(KnotVector([0, 0, 1, 1]))
 julia> domain(P)
 0..1
 
-julia> M = BSplineManifold([0,1], (P,));
+julia> M = BSplineManifold([0,1], P);
 
 julia> unbounded_mapping(M, 0.1)
 0.1
