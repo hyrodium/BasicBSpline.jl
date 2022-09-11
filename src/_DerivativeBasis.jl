6c6254f4cb138a10abbcfeb509e99a2d4a80cb16
hyrodium <hyrodium@gmail.com>
2022-09-11 19:28:26 +0900

    fix typo

--- src/_DerivativeBasis.jl
+++ src/_DerivativeBasis.jl
@@ -144,7 +144,7 @@ Modified version.
 \dot{B}_{(i,p,k)}(t)
 =p\left(\frac{1}{k_{i+p}-k_{i}}B_{(i,p-1,k)}(t)-\frac{1}{k_{i+p+1}-k_{i+1}}B_{(i+1,p-1,k)}(t)\right)
 ```
-`bsplinebasis′₊₀(P, i, t)` is equivalent to `bsplinebasis₊₀(derivative(P), i, t)`.
+`bsplinebasis′(P, i, t)` is equivalent to `bsplinebasis(derivative(P), i, t)`.
 """
 bsplinebasis′
 
