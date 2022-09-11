9753919411cc83b172b9160cfa51cad2df8d1995
hyrodium <hyrodium@gmail.com>
2022-09-11 17:23:34 +0900

    update docstring of bsplinebasis′

--- src/_DerivativeBasis.jl
+++ src/_DerivativeBasis.jl
@@ -109,33 +109,42 @@ end
 end
 
 @doc raw"""
+    bsplinebasis′₊₀(::AbstractFunctionSpace, ::Integer, ::Real) -> Real
+
 1st derivative of B-spline basis function.
 Right-sided limit version.
 ```math
 \dot{B}_{(i,p,k)}(t)
 =p\left(\frac{1}{k_{i+p}-k_{i}}B_{(i,p-1,k)}(t)-\frac{1}{k_{i+p+1}-k_{i+1}}B_{(i+1,p-1,k)}(t)\right)
 ```
+`bsplinebasis′₊₀(P, i, t)` is equivalent to `bsplinebasis₊₀(derivative(P), i, t)`.
 """
 bsplinebasis′₊₀
 
 
 @doc raw"""
+    bsplinebasis′₋₀(::AbstractFunctionSpace, ::Integer, ::Real) -> Real
+
 1st derivative of B-spline basis function.
 Left-sided limit version.
 ```math
 \dot{B}_{(i,p,k)}(t)
 =p\left(\frac{1}{k_{i+p}-k_{i}}B_{(i,p-1,k)}(t)-\frac{1}{k_{i+p+1}-k_{i+1}}B_{(i+1,p-1,k)}(t)\right)
 ```
+`bsplinebasis′₋₀(P, i, t)` is equivalent to `bsplinebasis₊₀(derivative(P), i, t)`.
 """
 bsplinebasis′₋₀
 
 @doc raw"""
+    bsplinebasis′(::AbstractFunctionSpace, ::Integer, ::Real) -> Real
+
 1st derivative of B-spline basis function.
 Modified version.
 ```math
 \dot{B}_{(i,p,k)}(t)
 =p\left(\frac{1}{k_{i+p}-k_{i}}B_{(i,p-1,k)}(t)-\frac{1}{k_{i+p+1}-k_{i+1}}B_{(i+1,p-1,k)}(t)\right)
 ```
+`bsplinebasis′₊₀(P, i, t)` is equivalent to `bsplinebasis₊₀(derivative(P), i, t)`.
 """
 bsplinebasis′
 
