a201d9c31dd646f01777d79f53bdfe6487768d11
hyrodium <hyrodium@gmail.com>
2023-12-17 12:53:20 +0900

    add tests for RationalBSplineManifold refinement

--- test/test_Refinement.jl
+++ test/test_Refinement.jl
@@ -29,9 +29,15 @@
             a1 = rand(SVector{3, Float64}, n1)
             a2 = rand(SVector{3, Float64}, n2)
             a3 = rand(SVector{3, Float64}, n3)
+            w1 = rand(n1) .+ 1
+            w2 = rand(n2) .+ 1
+            w3 = rand(n3) .+ 1
             M1 = BSplineManifold(a1, P1)
             M2 = BSplineManifold(a2, P2)
             M3 = BSplineManifold(a3, P3)
+            R1 = RationalBSplineManifold(a1, w1, P1)
+            R2 = RationalBSplineManifold(a2, w2, P2)
+            R3 = RationalBSplineManifold(a3, w3, P3)
             @test refinement_R(M1, P1′) == refinement(M1, P1′)
             @test refinement_R(M2, P2′) == refinement(M2, P2′)
             @test_throws DomainError refinement_R(M3, P3′)
@@ -41,28 +47,53 @@
             @test refinement_R(M1) == refinement_I(M1) == refinement(M1)
             @test refinement_R(M2) == refinement_I(M2) == refinement(M2)
             @test refinement_R(M3) == refinement_I(M3) == refinement(M3)
+            @test refinement_R(R1, P1′) == refinement(R1, P1′)
+            @test refinement_R(R2, P2′) == refinement(R2, P2′)
+            @test_throws DomainError refinement_R(R3, P3′)
+            @test_throws DomainError refinement_I(R1, P1′)
+            @test refinement_I(R2, P2′) == refinement(R2, P2′)
+            @test refinement_I(R3, P3′) == refinement(R3, P3′)
+            @test refinement_R(R1) == refinement_I(R1) == refinement(R1)
+            @test refinement_R(R2) == refinement_I(R2) == refinement(R2)
+            @test refinement_R(R3) == refinement_I(R3) == refinement(R3)
         end
 
         @testset "2dim" begin
             a12 = rand(SVector{3, Float64}, n1, n2)
             a23 = rand(SVector{3, Float64}, n2, n3)
+            w12 = rand(n1, n2) .+ 1
+            w23 = rand(n2, n3) .+ 1
             M12 = BSplineManifold(a12, P1, P2)
             M23 = BSplineManifold(a23, P2, P3)
+            R12 = RationalBSplineManifold(a12, w12, P1, P2)
+            R23 = RationalBSplineManifold(a23, w23, P2, P3)
             @test refinement_R(M12, P1′, P2′) == refinement(M12, P1′, P2′)
             @test_throws DomainError refinement_R(M23, P2′, P3′) == refinement(M23, P2′, P3′)
             @test_throws DomainError refinement_I(M12, P1′, P2′) == refinement(M12, P1′, P2′)
             @test refinement_I(M23, P2′, P3′) == refinement(M23, P2′, P3′)
+            @test refinement_R(R12, P1′, P2′) == refinement(R12, P1′, P2′)
+            @test_throws DomainError refinement_R(R23, P2′, P3′) == refinement(R23, P2′, P3′)
+            @test_throws DomainError refinement_I(R12, P1′, P2′) == refinement(R12, P1′, P2′)
+            @test refinement_I(R23, P2′, P3′) == refinement(R23, P2′, P3′)
         end
 
         @testset "4dim" begin
             a1122 = rand(SVector{3, Float64}, n1, n1, n2, n2)
             a2233 = rand(SVector{3, Float64}, n2, n2, n3, n3)
+            w1122 = rand(n1, n1, n2, n2) .+ 1
+            w2233 = rand(n2, n2, n3, n3) .+ 1
             M1122 = BSplineManifold(a1122, P1, P1, P2, P2)
             M2233 = BSplineManifold(a2233, P2, P2, P3, P3)
+            R1122 = RationalBSplineManifold(a1122, w1122, P1, P1, P2, P2)
+            R2233 = RationalBSplineManifold(a2233, w2233, P2, P2, P3, P3)
             @test refinement_R(M1122, P1′, P1′, P2′, P2′) == refinement(M1122, P1′, P1′, P2′, P2′)
             @test_throws DomainError refinement_R(M2233, P2′, P2′, P3′, P3′) == refinement(M2233, P2′, P2′, P3′, P3′)
             @test_throws DomainError refinement_I(M1122, P1′, P1′, P2′, P2′) == refinement(M12, P1′, P1′, P2′, P2′)
             @test refinement_I(M2233, P2′, P2′, P3′, P3′) == refinement(M2233, P2′, P2′, P3′, P3′)
+            @test refinement_R(R1122, P1′, P1′, P2′, P2′) == refinement(R1122, P1′, P1′, P2′, P2′)
+            @test_throws DomainError refinement_R(R2233, P2′, P2′, P3′, P3′) == refinement(R2233, P2′, P2′, P3′, P3′)
+            @test_throws DomainError refinement_I(R1122, P1′, P1′, P2′, P2′) == refinement(R12, P1′, P1′, P2′, P2′)
+            @test refinement_I(R2233, P2′, P2′, P3′, P3′) == refinement(R2233, P2′, P2′, P3′, P3′)
         end
     end
 
