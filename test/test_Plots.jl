e8b6dc1dafe1655265f271c1b879a4ce57b9c6ae
hyrodium <hyrodium@gmail.com>
2024-02-04 15:45:37 +0900

    use `savefig` in plot tests

--- test/test_Plots.jl
+++ test/test_Plots.jl
@@ -1,23 +1,30 @@
 @testset "Plots" begin
+    dir_out = joinpath(@__DIR__, "out_plot")
+    rm(dir_out; force=true, recursive=true)
+    mkpath(dir_out)
+
     @testset "B-spline space" begin
         p = 3
         k = KnotVector(0:3)+p*KnotVector([0,3])
         P = BSplineSpace{p}(k)
-        plot(P)
+        pl = plot(P)
+        savefig(pl, joinpath(dir_out, "bspline_space.png"))
     end
 
     @testset "Uniform B-spline space" begin
         p = 3
         k = UniformKnotVector(0:8)
         P = BSplineSpace{p}(k)
-        plot(P)
+        pl = plot(P)
+        savefig(pl, joinpath(dir_out, "bspline_space_uniform.png"))
     end
 
     @testset "Derivative B-spline space" begin
         p = 3
         k = UniformKnotVector(0:8)
         dP = BSplineDerivativeSpace{1}(BSplineSpace{p}(k))
-        plot(dP)
+        pl = plot(dP)
+        savefig(pl, joinpath(dir_out, "bspline_space_derivative.png"))
     end
 
     @testset "B-spline curve in 2d" begin
@@ -26,7 +33,8 @@
         k = KnotVector(0:3)+p*KnotVector([0,3])
         P = BSplineSpace{p}(k)
         M = BSplineManifold(a, P)
-        plot(M)
+        pl = plot(M)
+        savefig(pl, joinpath(dir_out, "bspline_curve_2d.png"))
     end
 
     @testset "B-spline curve in 3d" begin
@@ -35,27 +43,8 @@
         k = KnotVector(0:3)+p*KnotVector([0,3])
         P = BSplineSpace{p}(k)
         M = BSplineManifold(a, P)
-        plot(M)
-    end
-
-    @testset "Rational B-spline curve in 2d" begin
-        a = [SVector(0, 0), SVector(1, 1), SVector(2, -1), SVector(3, 0), SVector(4, -2), SVector(5, 1)]
-        w = rand(6)
-        p = 3
-        k = KnotVector(0:3)+p*KnotVector([0,3])
-        P = BSplineSpace{p}(k)
-        M = RationalBSplineManifold(a, w, P)
-        plot(M)
-    end
-
-    @testset "Rational B-spline curve in 3d" begin
-        a = [SVector(0, 0, 2), SVector(1, 1, 1), SVector(2, -1, 4), SVector(3, 0, 0), SVector(4, -2, 0), SVector(5, 1, 2)]
-        w = rand(6)
-        p = 3
-        k = KnotVector(0:3)+p*KnotVector([0,3])
-        P = BSplineSpace{p}(k)
-        M = RationalBSplineManifold(a, w, P)
-        plot(M)
+        pl = plot(M)
+        savefig(pl, joinpath(dir_out, "bspline_curve_3d.png"))
     end
 
     @testset "B-spline surface in 3d" begin
@@ -67,7 +56,30 @@
         n2 = dim(P2)
         a = [SVector(i,j,sin(i+j)) for i in 1:n1, j in 1:n2]
         M = BSplineManifold(a,(P1,P2))
-        plot(M)
+        pl = plot(M)
+        savefig(pl, joinpath(dir_out, "bspline_surface_3d.png"))
+    end
+
+    @testset "Rational B-spline curve in 2d" begin
+        a = [SVector(0, 0), SVector(1, 1), SVector(2, -1), SVector(3, 0), SVector(4, -2), SVector(5, 1)]
+        w = rand(6)
+        p = 3
+        k = KnotVector(0:3)+p*KnotVector([0,3])
+        P = BSplineSpace{p}(k)
+        M = RationalBSplineManifold(a, w, P)
+        pl = plot(M)
+        savefig(pl, joinpath(dir_out, "rational_bspline_curve_2d.png"))
+    end
+
+    @testset "Rational B-spline curve in 3d" begin
+        a = [SVector(0, 0, 2), SVector(1, 1, 1), SVector(2, -1, 4), SVector(3, 0, 0), SVector(4, -2, 0), SVector(5, 1, 2)]
+        w = rand(6)
+        p = 3
+        k = KnotVector(0:3)+p*KnotVector([0,3])
+        P = BSplineSpace{p}(k)
+        M = RationalBSplineManifold(a, w, P)
+        pl = plot(M)
+        savefig(pl, joinpath(dir_out, "rational_bspline_curve_3d.png"))
     end
 
     @testset "Rational B-spline surface in 3d" begin
@@ -80,6 +92,7 @@
         a = [SVector(i,j,sin(i+j)) for i in 1:n1, j in 1:n2]
         w = rand(n1,n2)
         M = RationalBSplineManifold(a,w,(P1,P2))
-        plot(M)
+        pl = plot(M)
+        savefig(pl, joinpath(dir_out, "rational_bspline_surface_3d.png"))
     end
 end
