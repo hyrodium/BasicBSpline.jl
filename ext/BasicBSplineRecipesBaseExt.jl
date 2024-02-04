40d33875841129af91eb1d69b7f471a16612fa0c
Yuto Horikawa <hyrodium@gmail.com>
2024-02-04 22:15:19 +0900

    More plotting recipes (#370)
    * add `_Manifold` union type in BasicBSplineRecipesBaseExt.jl
    
    * add `PlotAttributesContolPoints` struct in BasicBSplineRecipesBaseExt.jl
    
    * use `savefig` in plot tests
    
    * ignore output directory in test
    
    * add more attributes
    
    * remove unnecessary attributes
    
    * add plotting methods for `_Manifold{2, 2}`
    
    * add `division_number` to plotting methods
    
    * add missing `delete!` in plotting methods
    
    * add plotting method for BSplineSolid
    
    * fix boundaries for BSplineSolid
    
    * simplify plotting method for BSplineSolid
    
    * fix for Julia 1.6
    
    * add plotting method for knot vector
    
    * update default `division_number`
    
    * update plotting tests
    
    * update plotting tests
    
    * update plotting tests

--- ext/BasicBSplineRecipesBaseExt.jl
+++ ext/BasicBSplineRecipesBaseExt.jl
@@ -6,91 +6,207 @@ import BasicBSpline.AbstractFunctionSpace
 using StaticArrays
 
 # B-spline space
-@recipe function f(P::AbstractFunctionSpace)
-    # TODO fix number of sampling points
-    N = 100
+@recipe function f(k::AbstractKnotVector; shift_y=0.02)
+    u = BasicBSpline._vec(k)
+    l = length(u)
+    v = zeros(Float64, l)
+    for i in 2:l
+        if u[i-1] == u[i]
+            v[i] = v[i-1] - shift_y
+        end
+    end
+    seriestype := :scatter
+    delete!(plotattributes, :shift_y)
+    u, v
+end
+
+# B-spline space
+@recipe function f(P::AbstractFunctionSpace; division_number=20)
     k = knotvector(P)
     ts = Float64[]
     for i in 1:length(k)-1
-        append!(ts, range(k[i], k[i+1], length=N))
+        append!(ts, range(k[i], k[i+1], length=division_number+1))
     end
     unique!(ts)
     n = dim(P)
     tt = repeat([ts;NaN],n)
     bs = [bsplinebasis(P,i,t) for t in ts, i in 1:n]
     bb = vec(vcat(bs,zeros(n)'))
+    delete!(plotattributes, :division_number)
     tt, bb
 end
 
-# B-spline curve in 2d
-@recipe function f(M::Union{BSplineManifold{1,Deg,<:StaticVector{2,<:Real}}, RationalBSplineManifold{1,Deg,<:StaticVector{2,<:Real}}}) where Deg
-    # TODO fix number of sampling points
-    N = 100
+const _Manifold{Dim1, Dim2} = Union{BSplineManifold{Dim1,Deg,<:StaticVector{Dim2,<:Real}}, RationalBSplineManifold{Dim1,Deg,<:StaticVector{Dim2,<:Real}}} where Deg
+
+Base.@kwdef struct PlotAttributesContolPoints
+    # https://docs.juliaplots.org/latest/generated/attributes_series/
+    line_z=nothing
+    linealpha=nothing
+    linecolor=:gray
+    linestyle=:solid
+    linewidth=:auto
+    marker_z=nothing
+    markeralpha=nothing
+    markercolor=:gray
+    markershape=:circle
+    markersize=4
+    markerstrokealpha=nothing
+    markerstrokecolor=:match
+    markerstrokestyle=:solid
+    markerstrokewidth=1
+end
+
+@recipe function f(M::_Manifold{1, 2}; controlpoints=(;), division_number=100)
+    attributes = PlotAttributesContolPoints(;controlpoints...)
     t_min, t_max = extrema(domain(bsplinespaces(M)[1]))
-    ts = range(t_min, t_max, length=N)
+    ts = range(t_min, t_max, length=division_number+1)
     @series begin
         primary := false
-        linecolor := :gray
-        markershape := :circle
-        markercolor := :gray
-        a = controlpoints(M)
+        line_z := attributes.line_z
+        linealpha := attributes.linealpha
+        linecolor := attributes.linecolor
+        linestyle := attributes.linestyle
+        linewidth := attributes.linewidth
+        marker_z := attributes.marker_z
+        markeralpha := attributes.markeralpha
+        markercolor := attributes.markercolor
+        markershape := attributes.markershape
+        markersize := attributes.markersize
+        markerstrokealpha := attributes.markerstrokealpha
+        markerstrokecolor := attributes.markerstrokecolor
+        markerstrokestyle := attributes.markerstrokestyle
+        markerstrokewidth := attributes.markerstrokewidth
+        a = BasicBSpline.controlpoints(M)
         getindex.(a,1), getindex.(a,2)
     end
     p = M.(ts)
+    delete!(plotattributes, :controlpoints)
+    delete!(plotattributes, :division_number)
     getindex.(p,1), getindex.(p,2)
 end
 
-# B-spline curve in 3d
-@recipe function f(M::Union{BSplineManifold{1,Deg,<:StaticVector{3,<:Real}}, RationalBSplineManifold{1,Deg,<:StaticVector{3,<:Real}}}) where Deg
-    # TODO fix number of sampling points
-    N = 100
+@recipe function f(M::_Manifold{1, 3}; controlpoints=(;), division_number=100)
+    attributes = PlotAttributesContolPoints(;controlpoints...)
     t_min, t_max = extrema(domain(bsplinespaces(M)[1]))
-    ts = range(t_min, t_max, length=N)
+    ts = range(t_min, t_max, length=division_number+1)
     @series begin
         primary := false
-        linecolor := :gray
-        markershape := :circle
-        markercolor := :gray
-        a = controlpoints(M)
+        line_z := attributes.line_z
+        linealpha := attributes.linealpha
+        linecolor := attributes.linecolor
+        linestyle := attributes.linestyle
+        linewidth := attributes.linewidth
+        marker_z := attributes.marker_z
+        markeralpha := attributes.markeralpha
+        markercolor := attributes.markercolor
+        markershape := attributes.markershape
+        markersize := attributes.markersize
+        markerstrokealpha := attributes.markerstrokealpha
+        markerstrokecolor := attributes.markerstrokecolor
+        markerstrokestyle := attributes.markerstrokestyle
+        markerstrokewidth := attributes.markerstrokewidth
+        a = BasicBSpline.controlpoints(M)
         getindex.(a,1), getindex.(a,2), getindex.(a,3)
     end
     p = M.(ts)
+    delete!(plotattributes, :controlpoints)
+    delete!(plotattributes, :division_number)
     getindex.(p,1), getindex.(p,2), getindex.(p,3)
 end
 
-# B-spline surface
-@recipe function f(M::Union{BSplineManifold{2,Deg,<:StaticVector{3,<:Real}}, RationalBSplineManifold{2,Deg,<:StaticVector{3,<:Real}}}) where Deg
-    # TODO fix number of sampling points
-    N = 100
+@recipe function f(M::_Manifold{2, 2}; controlpoints=(;), division_number=100)
+    attributes = PlotAttributesContolPoints(;controlpoints...)
+    a = BasicBSpline.controlpoints(M)
+    @series begin
+        primary := false
+        marker_z := attributes.marker_z
+        markeralpha := attributes.markeralpha
+        markercolor := attributes.markercolor
+        markershape := attributes.markershape
+        markersize := attributes.markersize
+        markerstrokealpha := attributes.markerstrokealpha
+        markerstrokecolor := attributes.markerstrokecolor
+        markerstrokestyle := attributes.markerstrokestyle
+        markerstrokewidth := attributes.markerstrokewidth
+        seriestype := :scatter
+        getindex.(a,1), getindex.(a,2)
+    end
+    @series begin
+        primary := false
+        line_z := attributes.line_z
+        linealpha := attributes.linealpha
+        linecolor := attributes.linecolor
+        linestyle := attributes.linestyle
+        linewidth := attributes.linewidth
+        seriestype := :path
+        getindex.(a,1), getindex.(a,2)
+    end
+    @series begin
+        primary := false
+        line_z := attributes.line_z
+        linealpha := attributes.linealpha
+        linecolor := attributes.linecolor
+        linestyle := attributes.linestyle
+        linewidth := attributes.linewidth
+        seriestype := :path
+        getindex.(a',1), getindex.(a',2)
+    end
+    I1, I2 = domain.(bsplinespaces(M))
+    a1, b1 = float.(extrema(I1))
+    a2, b2 = float.(extrema(I2))
+    ts_boundary = [
+        [(t1, a2) for t1 in view(range(I1, length=division_number+1), 1:division_number)];
+        [(b1, t2) for t2 in view(range(I2, length=division_number+1), 1:division_number)];
+        [(t1, b2) for t1 in view(range(I1, length=division_number+1), division_number+1:-1:2)];
+        [(a1, t2) for t2 in view(range(I2, length=division_number+1), division_number+1:-1:1)]
+    ]
+    ps_boundary = [M(t...) for t in ts_boundary]
+    fill := true
+    fillalpha --> 0.5
+    delete!(plotattributes, :controlpoints)
+    delete!(plotattributes, :division_number)
+    getindex.(ps_boundary,1), getindex.(ps_boundary,2)
+end
+
+@recipe function f(M::_Manifold{2, 3}; controlpoints=(;), division_number=100)
+    attributes = PlotAttributesContolPoints(;controlpoints...)
     t1_min, t1_max = extrema(domain(bsplinespaces(M)[1]))
     t2_min, t2_max = extrema(domain(bsplinespaces(M)[2]))
-    t1s = range(t1_min, t1_max, length=N)
-    t2s = range(t2_min, t2_max, length=N)
+    t1s = range(t1_min, t1_max, length=division_number+1)
+    t2s = range(t2_min, t2_max, length=division_number+1)
+    a = BasicBSpline.controlpoints(M)
     @series begin
         primary := false
-        linecolor := :gray
-        markershape := :circle
-        markercolor := :gray
+        marker_z := attributes.marker_z
+        markeralpha := attributes.markeralpha
+        markercolor := attributes.markercolor
+        markershape := attributes.markershape
+        markersize := attributes.markersize
+        markerstrokealpha := attributes.markerstrokealpha
+        markerstrokecolor := attributes.markerstrokecolor
+        markerstrokestyle := attributes.markerstrokestyle
+        markerstrokewidth := attributes.markerstrokewidth
         seriestype := :scatter
-        a = controlpoints(M)
         getindex.(a,1), getindex.(a,2), getindex.(a,3)
     end
     @series begin
         primary := false
-        linecolor := :gray
-        markershape := :circle
-        markercolor := :gray
+        line_z := attributes.line_z
+        linealpha := attributes.linealpha
+        linecolor := attributes.linecolor
+        linestyle := attributes.linestyle
+        linewidth := attributes.linewidth
         seriestype := :path
-        a = controlpoints(M)
         getindex.(a,1), getindex.(a,2), getindex.(a,3)
     end
     @series begin
         primary := false
-        linecolor := :gray
-        markershape := :circle
-        markercolor := :gray
+        line_z := attributes.line_z
+        linealpha := attributes.linealpha
+        linecolor := attributes.linecolor
+        linestyle := attributes.linestyle
+        linewidth := attributes.linewidth
         seriestype := :path
-        a = controlpoints(M)
         getindex.(a',1), getindex.(a',2), getindex.(a',3)
     end
     ps = M.(t1s,t2s')
@@ -98,12 +214,107 @@ end
     ys = getindex.(ps,2)
     zs = getindex.(ps,3)
     seriestype := :surface
-    xs,ys,zs
+    delete!(plotattributes, :controlpoints)
+    delete!(plotattributes, :division_number)
+    xs, ys, zs
 end
 
-#=
-TODO
-* BSplineSolid
-=#
+@recipe function f(M::_Manifold{3, 3}; controlpoints=(;), division_number=100)
+    attributes = PlotAttributesContolPoints(;controlpoints...)
+    t1_min, t1_max = extrema(domain(bsplinespaces(M)[1]))
+    t2_min, t2_max = extrema(domain(bsplinespaces(M)[2]))
+    t3_min, t3_max = extrema(domain(bsplinespaces(M)[3]))
+    t1s = range(t1_min, t1_max, length=division_number+1)
+    t2s = range(t2_min, t2_max, length=division_number+1)
+    t3s = range(t3_min, t3_max, length=division_number+1)
+    a = BasicBSpline.controlpoints(M)
+    @series begin
+        primary := false
+        marker_z := attributes.marker_z
+        markeralpha := attributes.markeralpha
+        markercolor := attributes.markercolor
+        markershape := attributes.markershape
+        markersize := attributes.markersize
+        markerstrokealpha := attributes.markerstrokealpha
+        markerstrokecolor := attributes.markerstrokecolor
+        markerstrokestyle := attributes.markerstrokestyle
+        markerstrokewidth := attributes.markerstrokewidth
+        seriestype := :scatter
+        vec(getindex.(a,1)), vec(getindex.(a,2)), vec(getindex.(a,3))
+    end
+    @series begin
+        primary := false
+        line_z := attributes.line_z
+        linealpha := attributes.linealpha
+        linecolor := attributes.linecolor
+        linestyle := attributes.linestyle
+        linewidth := attributes.linewidth
+        seriestype := :path
+        n1, n2, n3 = dim.(bsplinespaces(M))
+        X = Float64[]
+        Y = Float64[]
+        Z = Float64[]
+        a1 = a
+        xs = [p[1] for p in a1]
+        ys = [p[2] for p in a1]
+        zs = [p[3] for p in a1]
+        append!(X, vec(cat(xs, fill(NaN,1,n2,n3), dims=1)))
+        append!(Y, vec(cat(ys, fill(NaN,1,n2,n3), dims=1)))
+        append!(Z, vec(cat(zs, fill(NaN,1,n2,n3), dims=1)))
+        a2 = permutedims(a,(2,3,1))
+        xs = [p[1] for p in a2]
+        ys = [p[2] for p in a2]
+        zs = [p[3] for p in a2]
+        append!(X, vec(cat(xs, fill(NaN,1,n3,n1), dims=1)))
+        append!(Y, vec(cat(ys, fill(NaN,1,n3,n1), dims=1)))
+        append!(Z, vec(cat(zs, fill(NaN,1,n3,n1), dims=1)))
+        a3 = permutedims(a,(3,1,2))
+        xs = [p[1] for p in a3]
+        ys = [p[2] for p in a3]
+        zs = [p[3] for p in a3]
+        append!(X, vec(cat(xs, fill(NaN,1,n1,n2), dims=1)))
+        append!(Y, vec(cat(ys, fill(NaN,1,n1,n2), dims=1)))
+        append!(Z, vec(cat(zs, fill(NaN,1,n1,n2), dims=1)))
+
+        X,Y,Z
+    end
+
+    nanvec = fill(SVector(NaN,NaN,NaN), 1, division_number+1)
+
+    qs1 = M.(t1s, t2s', t3_max)
+    qs2 = M.(t1s', t2s, t3_min)
+    qs3 = M.(t1_max, t2s, t3s')
+    qs4 = M.(t1_min, t2s', t3s)
+    qs5 = M.(t1s', t2_max, t3s)
+    qs6 = M.(t1s, t2_min, t3s')
+
+    ps = [
+        qs1;
+        qs1[end:end,:];  # Adhoc additional values for fixing boundary
+        nanvec;
+        qs2;
+        qs2[end:end,:];
+        nanvec;
+        qs3;
+        qs3[end:end,:];
+        nanvec;
+        qs4;
+        qs4[end:end,:];
+        nanvec;
+        qs5;
+        qs5[end:end,:];
+        nanvec;
+        qs6
+    ]
+
+    xs = getindex.(ps,1)
+    ys = getindex.(ps,2)
+    zs = getindex.(ps,3)
+
+    seriestype := :surface
+    delete!(plotattributes, :controlpoints)
+    delete!(plotattributes, :division_number)
+    xs, ys, zs
+end
 
 end # module
--- test/test_Plots.jl
+++ test/test_Plots.jl
@@ -1,23 +1,49 @@
 @testset "Plots" begin
+    dir_out = joinpath(@__DIR__, "out_plot")
+    rm(dir_out; force=true, recursive=true)
+    mkpath(dir_out)
+
     @testset "B-spline space" begin
         p = 3
         k = KnotVector(0:3)+p*KnotVector([0,3])
         P = BSplineSpace{p}(k)
-        plot(P)
+        pl = plot(P)
+        path_img = joinpath(dir_out, "bspline_space.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
+    end
+
+    @testset "B-spline space with knotvector" begin
+        k = knotvector"112111113111111121114"
+        P = BSplineSpace{3}(k)
+        pl = plot(P; label="B-spline basis"); plot!(k; label="knot vector")
+        path_img = joinpath(dir_out, "bspline_space_with_knotvector.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
     end
 
     @testset "Uniform B-spline space" begin
         p = 3
         k = UniformKnotVector(0:8)
         P = BSplineSpace{p}(k)
-        plot(P)
+        pl = plot(P)
+        path_img = joinpath(dir_out, "bspline_space_uniform.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
     end
 
     @testset "Derivative B-spline space" begin
         p = 3
         k = UniformKnotVector(0:8)
         dP = BSplineDerivativeSpace{1}(BSplineSpace{p}(k))
-        plot(dP)
+        pl = plot(dP)
+        path_img = joinpath(dir_out, "bspline_space_derivative.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
     end
 
     @testset "B-spline curve in 2d" begin
@@ -26,7 +52,11 @@
         k = KnotVector(0:3)+p*KnotVector([0,3])
         P = BSplineSpace{p}(k)
         M = BSplineManifold(a, P)
-        plot(M)
+        pl = plot(M)
+        path_img = joinpath(dir_out, "bspline_curve_2d.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
     end
 
     @testset "B-spline curve in 3d" begin
@@ -35,27 +65,27 @@
         k = KnotVector(0:3)+p*KnotVector([0,3])
         P = BSplineSpace{p}(k)
         M = BSplineManifold(a, P)
-        plot(M)
+        pl = plot(M)
+        path_img = joinpath(dir_out, "bspline_curve_3d.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
     end
 
-    @testset "Rational B-spline curve in 2d" begin
-        a = [SVector(0, 0), SVector(1, 1), SVector(2, -1), SVector(3, 0), SVector(4, -2), SVector(5, 1)]
-        w = rand(6)
-        p = 3
-        k = KnotVector(0:3)+p*KnotVector([0,3])
-        P = BSplineSpace{p}(k)
-        M = RationalBSplineManifold(a, w, P)
-        plot(M)
-    end
-
-    @testset "Rational B-spline curve in 3d" begin
-        a = [SVector(0, 0, 2), SVector(1, 1, 1), SVector(2, -1, 4), SVector(3, 0, 0), SVector(4, -2, 0), SVector(5, 1, 2)]
-        w = rand(6)
-        p = 3
-        k = KnotVector(0:3)+p*KnotVector([0,3])
-        P = BSplineSpace{p}(k)
-        M = RationalBSplineManifold(a, w, P)
-        plot(M)
+    @testset "B-spline surface in 2d" begin
+        k1 = KnotVector(1:8)
+        k2 = KnotVector(1:10)
+        P1 = BSplineSpace{2}(k1)
+        P2 = BSplineSpace{2}(k2)
+        n1 = dim(P1)
+        n2 = dim(P2)
+        a = [SVector(i+j/2-cos(i-j)/5, j-i/2+sin(i+j)/5) for i in 1:n1, j in 1:n2]
+        M = BSplineManifold(a,(P1,P2))
+        pl = plot(M)
+        path_img = joinpath(dir_out, "bspline_surface_2d.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
     end
 
     @testset "B-spline surface in 3d" begin
@@ -67,7 +97,54 @@
         n2 = dim(P2)
         a = [SVector(i,j,sin(i+j)) for i in 1:n1, j in 1:n2]
         M = BSplineManifold(a,(P1,P2))
-        plot(M)
+        pl = plot(M)
+        path_img = joinpath(dir_out, "bspline_surface_3d.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
+    end
+
+    @testset "B-spline solid in 3d" begin
+        P1 = P2 = BSplineSpace{3}(KnotVector([0,0,0,0,1,1,1,1]))
+        P3 = BSplineSpace{3}(KnotVector([0,0,0,0,1,2,3,4,5,6,6,6,6]))
+        n1 = dim(P1)
+        n2 = dim(P2)
+        n3 = dim(P3)
+        a = [SVector(i+j/2-cos(i-j)/5+sin(k), j-i/2+sin(i+j)/5+cos(k), k) for i in 1:n1, j in 1:n2, k in 1:n3]
+        M = BSplineManifold(a, P1, P2, P3);
+        pl = plot(M; controlpoints=(;markersize=1))
+        path_img = joinpath(dir_out, "bspline_solid_3d.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
+    end
+
+    @testset "Rational B-spline curve in 2d" begin
+        a = [SVector(0, 0), SVector(1, 1), SVector(2, -1), SVector(3, 0), SVector(4, -2), SVector(5, 1)]
+        w = rand(6)
+        p = 3
+        k = KnotVector(0:3)+p*KnotVector([0,3])
+        P = BSplineSpace{p}(k)
+        M = RationalBSplineManifold(a, w, P)
+        pl = plot(M)
+        path_img = joinpath(dir_out, "rational_bspline_curve_2d.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
+    end
+
+    @testset "Rational B-spline curve in 3d" begin
+        a = [SVector(0, 0, 2), SVector(1, 1, 1), SVector(2, -1, 4), SVector(3, 0, 0), SVector(4, -2, 0), SVector(5, 1, 2)]
+        w = rand(6)
+        p = 3
+        k = KnotVector(0:3)+p*KnotVector([0,3])
+        P = BSplineSpace{p}(k)
+        M = RationalBSplineManifold(a, w, P)
+        pl = plot(M)
+        path_img = joinpath(dir_out, "rational_bspline_curve_3d.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
     end
 
     @testset "Rational B-spline surface in 3d" begin
@@ -80,6 +157,10 @@
         a = [SVector(i,j,sin(i+j)) for i in 1:n1, j in 1:n2]
         w = rand(n1,n2)
         M = RationalBSplineManifold(a,w,(P1,P2))
-        plot(M)
+        pl = plot(M)
+        path_img = joinpath(dir_out, "rational_bspline_surface_3d.png")
+        @test !isfile(path_img)
+        savefig(pl, path_img)
+        @test isfile(path_img)
     end
 end
