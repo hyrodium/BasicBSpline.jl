8281e6646c9139dcba82f1abe59d28f1a55b5a86
Horikawa Yuto <hyrodium@gmail.com>
2025-03-31 14:21:50 +0900

    fix  test

--- test/test_RationalBSplineManifold.jl
+++ test/test_RationalBSplineManifold.jl
@@ -27,7 +27,7 @@
         @test M1 == M2 == RationalBSplineManifold(M1) == RationalBSplineManifold(M2)
         @test hash(M1) == hash(M2) == hash(RationalBSplineManifold(M1)) == hash(RationalBSplineManifold(M2))
         @test M3 ≠ M4 ≠ RationalBSplineManifold(M3) ≠ RationalBSplineManifold(M4)
-        @test hash(M3) ≠ hash(M4) ≠ hash(RationalBSplineManifold(M3)) ≠ hash(RationalBSplineManifold(M4))
+        @test hash(M3) ≠ hash(M4) == hash(RationalBSplineManifold(M3)) == hash(RationalBSplineManifold(M4))
         @test M1() == 1.2
         @test M2() == 1.2
         @test M3() == 1.2
