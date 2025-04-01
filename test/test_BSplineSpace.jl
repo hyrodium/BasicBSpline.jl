3d853c496e7fe8fd127abcdab0ce3a2a6d46c392
Horikawa Yuto <hyrodium@gmail.com>
2025-04-01 16:40:39 +0900

    update tests

--- test/test_BSplineSpace.jl
+++ test/test_BSplineSpace.jl
@@ -233,4 +233,22 @@
         @test P7 ⋥ P6
         @test domain(P6) == domain(P7)
     end
+
+    @testset "clamp" begin
+        P1 = BSplineSpace{2}(knotvector"2132111")
+        P2 = BSplineSpace{2}(knotvector" 3323")
+        @test clamp(P1) == P2 == clamp(P2)
+        @test !isclamped(P1)
+        @test isclamped(P2)
+        @test P1 ≠ P2
+        clamp!(P1)
+        @test isclamped(P1)
+        @test P1 == P2
+
+        P3 = BSplineSpace{3}(knotvector(0:10))
+        @test knotvector(P3) isa UniformKnotVector{Int}
+        @test_throws MethodError clamp!(P3)
+        @test knotvector(clamp(P3)) isa KnotVector{Int}
+        @test clamp(P3) == BSplineSpace{3}(knotvector([3,3,3,3,4,5,6,7,7,7,7]))
+    end
 end
